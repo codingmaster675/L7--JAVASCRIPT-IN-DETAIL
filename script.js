312143f0e04afc7884a0fa8bd8b8c940958ca74b
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-10-06 11:41:34 +0300

    JS in Detail

---
+++ script.js
@@ -0,0 +1,21 @@
+// null
+//string
+// number
+//object
+//array
+//boolean
+//undefined\
+var str=40
+var a=5
+
+document.write(str+a)
+var str=String(str)
+var a=String(a)
+document.write(str+a)
+
+try {
+    name()
+}
+catch (error){
+    document.write(error.message)
+}
